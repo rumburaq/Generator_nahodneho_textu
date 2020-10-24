64752e8516b9fc907e9ef6190dbbadef5167320f
rumburaq <60288604+rumburaq@users.noreply.github.com>
2020-10-24 13:07:35 +0200

    Update generator2.c
    testovani velikosti pole je to dole v komentech - hranice je [233]

--- generator2.c
+++ generator2.c
@@ -8,24 +8,22 @@ int randoms(int lower, int upper, int count);
 int main()
 {
     char pole[200];
-    int x = 0; // used for index in an array
-    int y = 1; // -||-
+    int x = 0; // used for index in an array //nebude potreba
+    int y = 1; // -||- //nebude potreba
     int lower; 
     int upper; 
     int count;
     int Ncislo; //random num
-    char z;
-    char c;
-    char PRz; //pervious letter
+    char z; 
+    char c; //nebude potreba
+    char PRz; //pervious letter  //nebude potreba
     int ps; //num of word in the text
-    int i;
+    int i; //nebude potreba
     int j;
-    
     
     srand(time(0)); //begin time (used for generating random num)
     printf("Enter the string:\n");
     scanf("%[^\n]s", pole);
-    
     
     for (j = 0; pole[j] != '\0';j++){ //zjisti ps
         
@@ -34,7 +32,6 @@ int main()
     }
     
     printf("poct slov: %d\n", ps + 1);
-    
     
     //gen Ncislo
     lower = 2, upper = ps, count = 1; //count vzdy na 1! 
@@ -59,9 +56,7 @@ int main()
         }
                 
     }
-    
     printf("\nPocet slov: %d", ps + 1);
-    
 }
 
 int randoms(int lower, int upper, int count) 
@@ -75,3 +70,20 @@ int randoms(int lower, int upper, int count)
     } 
     return num; 
 }
+
+//serazeno chronologicky jak jsem testoval
+//vstup na 121 znaku = 19 slov (1. čl. 1. pargraf ústavy čr)
+//pole = [2O0] - funguje jak ma
+//pole = [220] - funguje ale 1. pocet je o 1 vetsi nez ten pravy a ten druhy (2. je spravne)
+//pole = [230] -      -||-
+//
+//pole = [240] - pocet slov je -236604573 (nekdy je to kladné č.) takze Ncislo nema spravnou horni hranici
+//takze se nevypise to spravny slovo ale u 2. for cyklu se slova spocitaji spravne ↓
+//   //spocita spatne slova
+//   //nastavy moc vysoko Ncislo
+     //spravne pocita slova ale nevypise 58365156. slovo protoze tolik slov tam neni 
+//
+//pole = [235] -   -||- = nefunguje
+//pole = [231], [232] - stjně jako [220] a [230]
+//pole = [233] - nefunguje stejně jako [240] a [235]
+//kdyz jsem vstup prodlouzil na 23 slov = 141 znaků tak porad [233] nefunguje
